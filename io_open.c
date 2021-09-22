791ff1c2ceb918a4016c9bc4c2a80a30b0924764
Alvaro Gamez Machado <alvaro.gamez@hazent.com>
2021-09-22 15:44:53 +0200

    io_open: generic controller using open(), read() and write()

--- io_open.c
+++ io_open.c
@@ -22,26 +22,40 @@ static struct lart_io *io_open(struct lart_io_uri *uri)
 	if (!uri) 
 		return NULL;
 
-	struct io_open *io_open = calloc(1, sizeof(*io_open)); 
-	if (!io_open)
+	struct io_open *io = calloc(1, sizeof(*io)); 
+	if (!io)
 		return NULL;
 
-	io_open->art_io.ops = &io_open_ops;
-	io_open->fd = open(uri->parameters[0], O_RDWR | O_APPEND | O_CREAT, 0600);
+	io->art_io.ops = &io_open_ops;
+	io->fd = open(uri->parameters[0], O_RDWR | O_APPEND | O_CREAT, 0600);
+	if (io->fd < 0)
+		goto free_io;
 
-	return (struct lart_io *)io_open;
+	return (struct lart_io *)io;
+
+free_io:
+	free(io);
+	return NULL;
 }
 
-static int io_close(struct lart_io *io)
+static int io_close(struct lart_io *art_io)
 {
+	struct io_open *io = container_of(art_io, struct io_open, art_io);
+	int ret = close(io->fd);
+	free(io);
+	return ret;
 }
 
-static ssize_t io_write(struct lart_io *io, const void *buf, size_t count)
+static ssize_t io_write(struct lart_io *art_io, const void *buf, size_t count)
 {
+	struct io_open *io = container_of(art_io, struct io_open, art_io);
+	return write(io->fd, buf, count);
 }
 
-static ssize_t io_read(struct lart_io *io, void *buf, size_t count)
+static ssize_t io_read(struct lart_io *art_io, void *buf, size_t count)
 {
+	struct io_open *io = container_of(art_io, struct io_open, art_io);
+	return read(io->fd, buf, count);
 }
 
 const static struct lart_io_ops io_open_ops = {
